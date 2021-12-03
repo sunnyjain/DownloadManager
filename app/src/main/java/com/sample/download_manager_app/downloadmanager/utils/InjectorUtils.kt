03a1fce8d0b9ffe8b9267c431e532e3d2c9085b4
Sunny Jain <sunnyjain.sj8@gmail.com>
2021-12-04 03:12:53 +0530

    removes the interceptor. It was not working as expected and connection service was more than enough for now to achieve the requirement.

--- app/src/main/java/com/sample/download_manager_app/downloadmanager/utils/InjectorUtils.kt
+++ app/src/main/java/com/sample/download_manager_app/downloadmanager/utils/InjectorUtils.kt
@@ -3,7 +3,6 @@ package com.sample.download_manager_app.downloadmanager.utils
 import android.content.Context
 import com.sample.download_manager_app.downloadmanager.database.DownloadManagerDatabase
 import com.sample.download_manager_app.downloadmanager.repo.DownloadManagerRepository
-import com.sample.download_manager_app.downloadmanager.utils.networkutil.ConnectivityInterceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 
@@ -16,14 +15,9 @@ object InjectorUtils {
     }
 
     fun getRetrofitService(): Retrofit {
-
         return Retrofit.Builder()
             .baseUrl("https://file-examples.com/")
-            .client(OkHttpClient().apply {
-
-                 this.newBuilder().addInterceptor(ConnectivityInterceptor())
-                     .build()
-            })
+            .client(OkHttpClient())
             .build()
     }
 }
