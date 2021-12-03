28b62b0ab9e5c7553b4acf5262be768d38bd8c5f
Sunny Jain <sunnyjain.sj8@gmail.com>
2021-12-04 00:02:04 +0530

    defines task states.

---
+++ app/src/main/java/com/sample/download_manager_app/downloadmanager/core/TaskStates.kt
@@ -0,0 +1,15 @@
+package com.sample.download_manager_app.downloadmanager.core
+
+object TaskStates {
+   const val INIT        = 0
+   const val READY       = 1
+   const val DOWNLOADING = 2
+   const val PAUSED      = 3
+   const val DOWNLOAD_FINISHED = 4
+   const val END         = 5
+
+   //action states
+   const val PAUSING = 6
+   const val CANCEL = 7
+   const val RESUMING = 8
+}
